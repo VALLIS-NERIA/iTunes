fe720a11349ca1c95fbf5fb192cfa7676e430abc
VALLIS-NERIA <genius-littley@foxmail.com>
2015-11-22 18:33:41 +0800

    reflection

--- Console/Program.cs
+++ Console/Program.cs
@@ -8,10 +8,32 @@ using System.Diagnostics;
 using System.Net;
 using System.Xml;
 using iTunesLib;
+using System.Reflection;
 
 namespace iTunesConsole {
     class Program {
+        static Program() {
+            //这个绑定事件必须要在引用到TestLibrary1这个程序集的方法之前,注意是方法之前,不是语句之间,就算语句是在方法最后一行,在进入方法的时候就会加载程序集,如果这个时候没有绑定事件,则直接抛出异常,或者程序终止了
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+        }
+
+        static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args) {
+            //获取加载失败的程序集的全名
+            var assName = new AssemblyName(args.Name).FullName;
+            if (args.Name == "Interop.iTunesLib, Version=1.13.0.0, Culture=neutral, PublicKeyToken=null") {
+                //读取资源
+                using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("_Console.Resources." +
+
+      new AssemblyName(args.Name).Name + ".dll")) {
+                    var bytes = new byte[stream.Length];
+                    stream.Read(bytes, 0, (int)stream.Length);
+                    return Assembly.Load(bytes);//加载资源文件中的dll,代替加载失败的程序集
+                }
+            }
+            throw new DllNotFoundException(assName);
+        }
         static void Main(string[] args) {
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
             //调用iTunes
             iTunesLib.IiTunes iTunes;
             iTunes= new iTunesLib.iTunesAppClass();
@@ -65,7 +87,7 @@ namespace iTunesConsole {
                 try {
                     wClient.DownloadFile(node.InnerText, song.Location.Substring(0, song.Location.LastIndexOf(".")) + ".lrc");
                 }
-                catch (System.Net.WebException ex) {
+                catch (System.Exception ex) {
                     Console.WriteLine(ex.Message);
                 }
             }
